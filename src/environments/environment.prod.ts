93bb79c0aefb86014e6ef37b893183f8fcd73943
kate <katee241199@gmail.com>
2020-05-19 23:55:46 +0300

    fix api base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // base_url: 'http://localhost:4200',
   // api_base_url: 'http://localhost:8080',
-  api_base_url: 'https://portal-database.herokuapp.com',
+  api_base_url: 'https://secure-spire-04164.herokuapp.com',
   // OAUTH2_REDIRECT_URI: 'http://localhost:4200/oauth2/redirect',
   // GOOGLE_AUTH_URL: this.API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + this.OAUTH2_REDIRECT_URI,
   ACCESS_TOKEN: 'accessToken'
